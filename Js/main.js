fd1825bed6f9f0da46c4cf8ebd262adf95d23ea7
idungstanley <sundaystanley56@gmail.com>
2023-10-16 13:12:15 +0100

    added code for hide on scroll

--- Js/main.js
+++ Js/main.js
@@ -7,4 +7,11 @@ styleSwitcherToggle.addEventListener('click', ()=>{
    const styleSwitcher = document.querySelector('.style-switcher').classList.toggle('open')
 })
 
-// 
+
+// hide toggle switcher on scroll
+
+window.addEventListener('scroll', ()=>{
+    if(document.querySelector('.style-switcher').classList.contains('open')){
+        document.querySelector('.style-switcher').classList.remove('open')
+    }
+})
