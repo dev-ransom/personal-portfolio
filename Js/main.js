19f3599f04d4e2a5ff6fc123bcc911ff140efb32
idungstanley <sundaystanley56@gmail.com>
2023-10-18 22:11:20 +0100

    added code for hiding of aside on small screen

--- Js/main.js
+++ Js/main.js
@@ -74,7 +74,13 @@ for (let i = 0; i < totalNavList; i++){
   const a =  navList[i].querySelector('a');
 
   a.addEventListener('click', function(){
+    for(let i = 0; i < totalSection; i++){
+        allSection[i].classList.remove('back-section');
+    }
     for(let j = 0; j < totalNavList; j++){
+        if(navList[j].querySelector('a').classList.contains('active')){
+            allSection[j].classList.add('back-section')
+        }
       navList[j].querySelector('a').classList.remove('active')
     }
     this.classList.add('active');
